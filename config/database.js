4bdf7fd825838846fede79f651b8fea63ec5160e
Tharun-06 <145473589+Tharun-06@users.noreply.github.com>
2024-02-24 13:49:41 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'mongodb://localhost/aswanth' 
+	'url' : 'mongodb+srv://easygo:<password>@easygo.d8ievy3.mongodb.net/?retryWrites=true&w=majority&appName=easygo' 
 
 };
